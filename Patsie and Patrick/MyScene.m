f7ad3d0f833ff5c122a29818ed9fa8083e4f1b3a
Crystal Tong <tongc312@newschool.edu>
2014-04-07 23:03:14 -0400

    deleted all joints

--- Patsie and Patrick/MyScene.m
+++ Patsie and Patrick/MyScene.m
@@ -8,134 +8,39 @@
 
 #import "MyScene.h"
 
+typedef NS_OPTIONS(uint32_t, CNPhysicsCategory)
+{
+    CNPhysicsCategoryCat = 1 << 0, //0001 = 1, pg. 245
+    CNPhysicsCategoryBlock = 1 << 1, //0010 = 2
+    CNPhysicsCategoryBed = 1 << 2, //0100 = 4
+};
 
 @interface MyScene()
-@property SKSpriteNode* myCircle1;
-@property SKSpriteNode* myCircle2;
-@property SKSpriteNode* myCircle3;
-@property SKSpriteNode* myCircle4;
-@property SKSpriteNode* myCircle5;
-@property SKSpriteNode* myCircle6;
-@property SKSpriteNode* myCircle7;
-@property SKSpriteNode* myCircle8;
-@property SKPhysicsJoint* myRopeJoint;
-@property SKPhysicsJoint* myRopeJoint1;
-@property SKPhysicsJoint* myRopeJoint2;
-@property SKPhysicsJoint* myRopeJoint3;
-@property SKPhysicsJoint* myRopeJoint4;
-@property SKPhysicsJoint* myRopeJoint5;
-@property SKPhysicsJoint* myRopeJoint6;
+
+@property SKSpriteNode* myCircle9;
+
 
 @end
 
 @implementation MyScene
 
--(void) activateJointRope{
-    
-    _myRopeJoint = [SKPhysicsJointLimit jointWithBodyA:_myCircle1.physicsBody bodyB:_myCircle2.physicsBody anchorA:_myCircle1.position anchorB:_myCircle2.position];
-    
-    [self.physicsWorld addJoint:_myRopeJoint];
-    
-    _myRopeJoint1 = [SKPhysicsJointLimit jointWithBodyA:_myCircle1.physicsBody bodyB:_myCircle3.physicsBody anchorA:_myCircle1.position anchorB:_myCircle3.position];
-    
-    [self.physicsWorld addJoint:_myRopeJoint1];
-    
-    
-    _myRopeJoint2 = [SKPhysicsJointLimit jointWithBodyA:_myCircle1.physicsBody bodyB:_myCircle4.physicsBody anchorA:_myCircle1.position anchorB:_myCircle4.position];
-    
-    [self.physicsWorld addJoint:_myRopeJoint2];
-    
-    _myRopeJoint3 = [SKPhysicsJointLimit jointWithBodyA:_myCircle1.physicsBody bodyB:_myCircle5.physicsBody anchorA:_myCircle1.position anchorB:_myCircle5.position];
-    
-    [self.physicsWorld addJoint:_myRopeJoint3];
-    
-    _myRopeJoint4 = [SKPhysicsJointLimit jointWithBodyA:_myCircle1.physicsBody bodyB:_myCircle6.physicsBody anchorA:_myCircle1.position anchorB:_myCircle6.position];
-    
-    [self.physicsWorld addJoint:_myRopeJoint4];
-    
-    _myRopeJoint5 = [SKPhysicsJointSpring jointWithBodyA:_myCircle1.physicsBody bodyB:_myCircle7.physicsBody anchorA:_myCircle1.position anchorB:_myCircle7.position];
-    
-    [self.physicsWorld addJoint:_myRopeJoint5];
-    
-    _myRopeJoint6 = [SKPhysicsJointLimit jointWithBodyA:_myCircle1.physicsBody bodyB:_myCircle8.physicsBody anchorA:_myCircle1.position anchorB:_myCircle8.position];
-    
-    [self.physicsWorld addJoint:_myRopeJoint6];
-    
-}
 
--(void) spawnCircles{
-    _myCircle1 = [[SKSpriteNode alloc]initWithColor:[SKColor redColor] size:CGSizeMake(30, 30)];
-    _myCircle2 =[[SKSpriteNode alloc]initWithColor:[SKColor purpleColor] size:CGSizeMake(10, 10)];
-    _myCircle3 =[[SKSpriteNode alloc]initWithColor:[SKColor orangeColor] size:CGSizeMake(10, 10)];
-    _myCircle4 =[[SKSpriteNode alloc]initWithColor:[SKColor yellowColor] size:CGSizeMake(10, 10)];
-    _myCircle5 =[[SKSpriteNode alloc]initWithColor:[SKColor redColor] size:CGSizeMake(10, 10)];
-    _myCircle6 =[[SKSpriteNode alloc]initWithColor:[SKColor purpleColor] size:CGSizeMake(10, 10)];
-    _myCircle7 =[[SKSpriteNode alloc]initWithColor:[SKColor orangeColor] size:CGSizeMake(10, 10)];
-    _myCircle8 =[[SKSpriteNode alloc]initWithColor:[SKColor yellowColor] size:CGSizeMake(10, 10)];
-    
-    
-//    [_myCircle1 setPosition:CGPointMake(self.size.width/1.5, self.size.height/1.5)];
-//    [_myCircle2 setPosition:CGPointMake(self.size.width/1.0, self.size.height/2)];
-//    [_myCircle3 setPosition:CGPointMake(self.size.width/1.0, self.size.height/1)];
-//    [_myCircle4 setPosition:CGPointMake(self.size.width/2.0, self.size.height/0.5)];
-//    [_myCircle5 setPosition:CGPointMake(self.size.width/2.5, self.size.height/2.5)];
-//    [_myCircle6 setPosition:CGPointMake(self.size.width/0.5, self.size.height/0.5)];
-//    [_myCircle7 setPosition:CGPointMake(self.size.width/0.5, self.size.height/2.5)];
-//    [_myCircle8 setPosition:CGPointMake(self.size.width/3, self.size.height/0.5)];
 
-    [_myCircle1 setPosition:CGPointMake(200, 200)];
-    [_myCircle2 setPosition:CGPointMake(220, 200)];
-    [_myCircle3 setPosition:CGPointMake(200, 220)];
-    [_myCircle4 setPosition:CGPointMake(220, 200)];
-    [_myCircle5 setPosition:CGPointMake(200, 220)];
-    [_myCircle6 setPosition:CGPointMake(200, 220)];
-    [_myCircle7 setPosition:CGPointMake(200, 220)];
-    [_myCircle8 setPosition:CGPointMake(200, 220)];
-
+-(void) presents{
     
-    _myCircle1.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:15];
-    _myCircle2.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:5];
-    _myCircle3.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:5];
-    _myCircle4.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:5];
-    _myCircle5.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:5];
-    _myCircle6.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:5];
-    _myCircle7.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:5];
-    _myCircle8.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:5];
+    _myCircle9 =[[SKSpriteNode alloc]initWithColor:[SKColor yellowColor] size:CGSizeMake(30, 30)];
     
-    [_myCircle1.physicsBody setRestitution:1.0];
-    [_myCircle2.physicsBody setRestitution:0];
-    [_myCircle3.physicsBody setRestitution:0];
-    [_myCircle4.physicsBody setRestitution:1.0];
-    [_myCircle5.physicsBody setRestitution:1.0];
-    [_myCircle6.physicsBody setRestitution:1.0];
-    [_myCircle7.physicsBody setRestitution:1.0];
-    [_myCircle8.physicsBody setRestitution:1.0];
+    [_myCircle9 setPosition:CGPointMake(300, 200)];
     
-    [self addChild:_myCircle1];
-    [_myCircle1 addChild:_myCircle2];
-    [_myCircle1 addChild:_myCircle3];
-    [_myCircle1 addChild:_myCircle4];
-    [_myCircle1 addChild:_myCircle5];
-    [_myCircle1 addChild:_myCircle6];
-    [_myCircle1 addChild:_myCircle7];
-    [_myCircle1 addChild:_myCircle8];
+    _myCircle9.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:_myCircle9.size];
     
-    [self activateJointRope];
-    
+    [_myCircle9.physicsBody setDynamic:YES];
+    [self addChild:_myCircle9];
 
     
 }
 
 
-//-(void)makeShelf1 {
-//    _myShelf1 = [[SKSpriteNode alloc]initWithColor:[SKColor lightGrayColor] size:CGSizeMake(100, 40)];
-//    _myShelf1.position = CGPointMake(self.size.width/1.4, self.size.height/4);
-//    _myShelf1.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:_myShelf1.size];
-//    [_myShelf1.physicsBody setDynamic:NO];
-//
-//    [self addChild:_myShelf1];
-//
-//}
 
 -(id)initWithSize:(CGSize)size {
     if (self = [super initWithSize:size]) {
@@ -146,59 +51,98 @@
         
       //  self.physicsBody = [SKPhysicsBody bodyWithEdgeLoopFromRect:self.frame];
         self.physicsBody = [SKPhysicsBody bodyWithEdgeLoopFromRect:self.frame];
-        [self.physicsBody setRestitution:1];
+        [self.physicsBody setRestitution:0.0];
         
         
-        [self spawnCircles];
-              [self spawnCircles];
+        [self presents];
         
         
         
     }
     return self;
 }
+-(void)addBlocksFromArray:(NSArray*)blocks
+{
+    //1 pg.241 this creates a block sprite for you
+    for (NSDictionary *block in blocks) {
+        
+        //2
+        SKSpriteNode *blockSprite =
+        [self addBlockWithRect:CGRectFromString(block[@"rect"])];
+        
+        //have categories of blocks and cat to collide pg 245
+        
+        blockSprite.physicsBody.categoryBitMask =
+        CNPhysicsCategoryBlock | CNPhysicsCategoryCat;
+        
+        blockSprite.physicsBody.collisionBitMask =
+        CNPhysicsCategoryBlock;
+        
+        
+    }// forNSDictionary
+}//addblocksarray
 
--(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
-    /* Called when a touch begins */
+-(SKSpriteNode*)addBlockWithRect:(CGRect)blockRect
+
+//whats the difference between CGRect and block rect?
+
+{
+    //3
+    NSString *textureName = [NSString stringWithFormat:@"%.fx%.f.png",blockRect.size.width, blockRect.size.height];
+    //4
+    SKSpriteNode *blockSprite =
+    [SKSpriteNode spriteNodeWithImageNamed:textureName];
+    blockSprite.position = blockRect.origin;
     
-    if (_myCircle1.physicsBody.dynamic) {
+    //5 make blockrect smaller by 2x2 = body rect,
+    
+    CGRect bodyRect = CGRectInset(blockRect, 2, 2);
+    blockSprite.physicsBody =
+    [SKPhysicsBody bodyWithRectangleOfSize:bodyRect.size];
+    
+    //6
+    //[blockSprite attachDebugRectWithSize:blockSprite.size];
+    
+    return blockSprite;
+}//blockwithrect pg.242
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+    {
+        [super touchesBegan:touches withEvent:event];
         
-        [_myCircle1.physicsBody setDynamic:NO];
-    }
-    
-    for (UITouch *touch in touches) {
+        // 1
+        UITouch *touch = [touches anyObject];
         CGPoint location = [touch locationInNode:self];
-        [_myCircle1 setPosition:location];
         
+        [_myCircle9 removeFromParent];
+        
+        // 2
+        [self.physicsWorld enumerateBodiesAtPoint:location
+                                       usingBlock:
+         ^(SKPhysicsBody *body, BOOL *stop) {
+             // 3
+             if (body.categoryBitMask == CNPhysicsCategoryBlock) {
+                 [body.node removeFromParent];
+                 *stop = YES; // 4
+                 
+                 // 5
+                 [self runAction:[SKAction playSoundFileNamed:@"pop.mp3"
+                                            waitForCompletion:NO]];
+             }
+         }];
         
     }
-}
 
 -(void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
-    for (UITouch *touch in touches) {
-        CGPoint location = [touch locationInNode:self];
-        [_myCircle1 setPosition:location];
-        
     }
-}
 
 -(void) touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event  {
-    
-    if (!_myCircle1.physicsBody.dynamic) {
-        [_myCircle1.physicsBody setDynamic:YES];
-    }
     
 }
 
 -(void) touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event{
     
-    if (!_myCircle1.physicsBody.dynamic) {
-        [_myCircle1.physicsBody setDynamic:YES];
     }
-    //  [_mySquare1.physicsBody setDynamic:YES];
-    //  [_mySquare2.physicsBody setDynamic:YES];
-    
-}
 
 
 -(void)update:(CFTimeInterval)currentTime {
